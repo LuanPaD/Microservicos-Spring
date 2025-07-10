b06c52e1892d6feede895eff46c660652548c557
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-10 19:00:10 -0300

    feat: Criaçao de UsuarioService  com operações de CRUD

---
+++ cadastro-usuario/src/main/java/com/luanmiranda/cadastro_usuario/business/UsuarioService.java
@@ -0,0 +1,40 @@
+package com.luanmiranda.cadastro_usuario.business;
+
+import com.luanmiranda.cadastro_usuario.api.converter.UsuarioConverter;
+import com.luanmiranda.cadastro_usuario.api.request.UsuarioRequestDTO;
+import com.luanmiranda.cadastro_usuario.api.response.UsuarioResponseDTO;
+import com.luanmiranda.cadastro_usuario.infrastructure.entities.UsuarioEntity;
+import com.luanmiranda.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioEntity cadastrarUsuario(UsuarioEntity usuarioEntity) {
+        return usuarioRepository.saveAndFlush(usuarioEntity);
+    }
+
+    public UsuarioResponseDTO gravarUsuario(UsuarioRequestDTO usuarioRequestDTO) {
+        try {
+            UsuarioEntity usuarioEntity = cadastrarUsuario(usuarioConverter.paraUsuarioEntity(usuarioRequestDTO));
+            return usuarioConverter.paraUsuarioResponseDTO(usuarioEntity);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao gravar dados de usuário", e);
+        }
+    }
+
+    public UsuarioResponseDTO buscaDadosUsuario(String email) {
+        return usuarioConverter.paraUsuarioResponseDTO(
+            usuarioRepository.findByEmail(email)
+        );
+    }
+
+    public void excluirUsuario(String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
+}
