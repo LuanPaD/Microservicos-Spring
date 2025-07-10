c9c8afa5a7b8ef82ed86aa58b9fe21a450ca98fa
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-10 19:01:17 -0300

    feat: Add metódos find e delete by email para o UsuarioRepository

--- cadastro-usuario/src/main/java/com/luanmiranda/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ cadastro-usuario/src/main/java/com/luanmiranda/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,7 +1,13 @@
 package com.luanmiranda.cadastro_usuario.infrastructure.repository;
 
 import com.luanmiranda.cadastro_usuario.infrastructure.entities.UsuarioEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+
+    UsuarioEntity findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
