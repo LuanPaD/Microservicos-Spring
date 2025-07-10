18fe56039343cfc8e8ac2e815dad6a4b53bca409
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-10 18:58:20 -0300

    refactor: alteração do tipo de endereco do  UsuarioRequestDTO para EnderecoRequestDTO

--- cadastro-usuario/src/main/java/com/luanmiranda/cadastro_usuario/api/request/UsuarioRequestDTO.java
+++ cadastro-usuario/src/main/java/com/luanmiranda/cadastro_usuario/api/request/UsuarioRequestDTO.java
@@ -12,5 +12,5 @@ public class UsuarioRequestDTO {
     private String nome;
     private String email;
     private String documento;
-    private EnderecoEntity endereco;
+    private EnderecoRequestDTO endereco;
 }
