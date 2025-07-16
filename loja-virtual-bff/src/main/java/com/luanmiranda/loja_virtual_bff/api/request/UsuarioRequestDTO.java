3b27fd252195c322ba730ee96f8c367dcf2f6d77
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-16 14:02:36 -0300

    feat: Criação de DTO's de request e response

---
+++ loja-virtual-bff/src/main/java/com/luanmiranda/loja_virtual_bff/api/request/UsuarioRequestDTO.java
@@ -0,0 +1,15 @@
+package com.luanmiranda.loja_virtual_bff.api.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioRequestDTO {
+    private String nome;
+    private String email;
+    private String documento;
+    private EnderecoRequestDTO endereco;
+}
---
+++ loja-virtual-bff/src/main/java/com/luanmiranda/loja_virtual_bff/api/response/EnderecoResponseDTO.java
@@ -0,0 +1,17 @@
+package com.luanmiranda.loja_virtual_bff.api.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EnderecoResponseDTO {
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String complemento;
+    private String cidade;
+    private String cep;
+}
